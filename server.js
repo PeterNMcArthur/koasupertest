f88626cce18ca5c998f4016829e5cf2c36cb06fb
Peter McArthur <petermcarthur@peters-imac.sportssupltd.local>
2016-02-26 18:01:12 +0000

    got connection working inside container

--- server.js
+++ server.js
@@ -2,6 +2,32 @@ var logger = require('koa-logger');
 var route = require('koa-router')();
 var koa = require('koa');
 var app = koa();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://192.168.99.100:27017/people');
+
+var db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+
+  var peopleSchema = mongoose.Schema({
+    name: String
+  }, { collection: "people"});
+
+  var people = mongoose.model('people', peopleSchema);
+
+  var james = new people({
+    name: "Tonny"
+  });
+
+  james.save(function (err) {if (err) console.log ('Error on save!')});
+
+  people.find(function(err, person) {
+    console.log(person);
+  });
+
+});
 
 route.get('/user', function *() {
 
@@ -21,6 +47,8 @@ route.get('/dude', function *() {
 
 });
 
+app.listen(9000);
+
 app.use(route.routes());
 
-module.exports = app;
+// module.exports = app;
